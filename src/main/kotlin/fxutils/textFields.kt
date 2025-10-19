0181b260d23bb4a0ab6ff48c51fa294a6060e587
Nikolaus Knop <niko.knop003@gmail.com>
2025-10-19 17:10:14 +0200

    Add method to sync `TextField` with reactive variable and indicate unsaved changes visually
    - Add `TextField.sync` extension function to bind text with a `ReactiveVariable`
    - Highlight unsaved changes with the "dirty" pseudo-class
    - Update styles to show green text for unsaved (`TextField:dirty`) changes

--- src/main/kotlin/fxutils/textFields.kt
+++ src/main/kotlin/fxutils/textFields.kt
@@ -3,6 +3,11 @@ package fxutils
 import javafx.scene.control.TextField
 import javafx.scene.text.Font
 import javafx.scene.text.Text
+import reaktive.value.ReactiveVariable
+import reaktive.value.fx.asReactiveValue
+import reaktive.value.getValue
+import reaktive.value.now
+import reaktive.value.observe
 
 fun TextField.autoSize(condition: () -> Boolean = { true }, minColumns: () -> Int = { 1 }) {
     val helper = Text()
@@ -36,4 +41,17 @@ private fun Text.computeTextWidth(text: String, font: Font): Double {
     this.font = font
     val textWidth = prefWidth(-1.0) + 3.0
     return textWidth
+}
+
+fun TextField.sync(variable: ReactiveVariable<String>) {
+    text = variable.now
+    setOnAction {
+        variable.now = text
+        setPseudoClassState("dirty", false)
+    }
+    textProperty().addListener { _, _, text ->
+        if (text != variable.now) {
+            setPseudoClassState("dirty", true)
+        }
+    }
 }
